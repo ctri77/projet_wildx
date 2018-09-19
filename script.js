4ebdfce238eb65821ec2540cc3cb1d16cdddeb67
Caroline TRI <caroline.tri77@gmail.com>
2018-09-19 09:01:21 +0200

    Modif navbar Caro

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-$('.navbar-collapse .navbar-nav > .nav-item > .nav-link').click(function(){
+$('.navbar-collapse .navbar-nav > .nav-item > .titi').click(function(){
+    $('.collapse.show').removeClass('show').css('height', '0');
+    });
+$('.navbar-collapse .navbar-nav > .nav-item > .dropdown-menu > .dropdown-item').click(function(){
     $('.collapse.show').removeClass('show').css('height', '0');
     });
