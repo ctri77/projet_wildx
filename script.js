06c7039ed75b8a99bd3ef702e2a1cf91cb5a0cf4
Alexandre <buissonalexandre16@gmail.com>
2018-09-21 11:41:32 +0200

    Correction indentation et ajout de commentaires

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-$('.navbar-collapse .navbar-nav > .nav-item > .titi').click(function(){
+
+//Fonction fermeture des menus après avoir cliqué 
+$('.navbar-collapse .navbar-nav > .nav-item > .titi').click(function () {
     $('.collapse.show').removeClass('show').css('height', '0');
-    });
-$('.navbar-collapse .navbar-nav > .nav-item > .dropdown-menu > .dropdown-item').click(function(){
+});
+$('.navbar-collapse .navbar-nav > .nav-item > .dropdown-menu > .dropdown-item').click(function () {
     $('.collapse.show').removeClass('show').css('height', '0');
-    });
+});
